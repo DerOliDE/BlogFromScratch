a76b998f2bf836d618d80dfaccee4115379cc120
DerOli <git@deroli.de>
2025-01-14 11:24:33 +0100

    Added Pebble as templating engine

--- src/main/kotlin/cloud/dataflows/blog/Routing.kt
+++ src/main/kotlin/cloud/dataflows/blog/Routing.kt
@@ -1,13 +1,17 @@
 package cloud.dataflows.blog
 
 import io.ktor.server.application.*
+import io.ktor.server.pebble.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val params = mapOf(
+                "name" to "World"
+            )
+            call.respond(PebbleContent("index.html", params))
         }
     }
 }
