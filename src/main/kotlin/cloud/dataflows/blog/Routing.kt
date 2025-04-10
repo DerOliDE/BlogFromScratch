074fc69d6e111482526335f0e688abbeeb708c46
DerOli <git@deroli.de>
2025-04-10 09:47:36 +0200

    Added HTMX for dynamic web stuff

--- src/main/kotlin/cloud/dataflows/blog/Routing.kt
+++ src/main/kotlin/cloud/dataflows/blog/Routing.kt
@@ -1,17 +1,29 @@
 package cloud.dataflows.blog
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.pebble.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
+import java.nio.file.Paths
 
 fun Application.configureRouting() {
     routing {
+
+        staticResources("/static", "static") {
+            extensions("js")
+        }
+
         get("/") {
+            call.respond(PebbleContent("index.html", emptyMap()))
+        }
+
+        post("/greetings") {
             val params = mapOf(
                 "name" to "World"
             )
-            call.respond(PebbleContent("index.html", params))
+            call.respond(PebbleContent("components/greetingComponent.html", params))
         }
     }
 }
